6aac5cbdaf7c9a5888d95b11eb5b9ac1abcf3327
Matteo Pellegrino <matteo.pelle.pellegrino@gmail.com>
2026-01-03 17:04:45 +0100

    refactor: Cleanup cards style

--- src/App.tsx
+++ src/App.tsx
@@ -220,6 +220,7 @@ export default function App() {
             value={currentEntry?.notes || ""}
             onChange={updateNotes}
             disabled={!hasData}
+            className="flex-1"
           />
 
           <Controls
