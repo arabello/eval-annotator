f72b0f007da6ddaa21ea8f5dec0789a8d55014ea
Matteo Pellegrino <matteo.pelle.pellegrino@gmail.com>
2026-01-03 17:04:45 +0100

    feat: Add GHA build and publish workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/eval-annotator/",
   plugins: [react(), tailwindcss()],
   resolve: {
     extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
